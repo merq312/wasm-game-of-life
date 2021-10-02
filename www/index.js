cf8fbe9e8453dc1779c2f30a91b559cde4318587
Chamila Gunasena <merc312@gmail.com>
2021-10-02 19:57:23 -0400

    feat: minor css changes

--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 import { Universe, Cell } from 'wasm-game-of-life'
 import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg'
 
-const CELL_SIZE = 5 // px
+const CELL_SIZE = 7 // px
 const GRID_COLOR = '#CCCCCC'
 const DEAD_COLOR = '#FFFFFF'
 const ALIVE_COLOR = '#000000'
@@ -69,7 +69,7 @@ let animationId = null
 
 const renderLoop = () => {
   // Uncomment to pause between each tick
-  // debugger;
+  // debugger
   drawGrid()
   drawCells()
 
