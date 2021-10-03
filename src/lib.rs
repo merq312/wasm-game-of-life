703f67ce69f1dc74626db3ebc74f2cda40f77692
Chamila Gunasena <merc312@gmail.com>
2021-10-02 23:34:50 -0400

    fix: performance improvements
    remove call to ctx.fillStyle for each individual cell and instead call it twice per tick for alive
    and dead cells

--- src/lib.rs
+++ src/lib.rs
@@ -69,6 +69,8 @@ impl Universe {
     }
 
     pub fn tick(&mut self) {
+        let _timer = Timer::new("Universe::tick");
+
         let mut next = self.cells.clone();
 
         for row in 0..self.height {
@@ -216,3 +218,21 @@ impl fmt::Display for Universe {
 }
 
 extern crate web_sys;
+use web_sys::console;
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
